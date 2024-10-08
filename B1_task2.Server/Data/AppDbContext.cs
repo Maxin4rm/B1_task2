38474bf415858a3ac606f3665ab9f7ce4772cd14
Maxin4rm <92393477+Maxin4rm@users.noreply.github.com>
2024-10-08 09:55:59 +0300

    Update AppDbContext.cs

--- B1_task2.Server/Data/AppDbContext.cs
+++ B1_task2.Server/Data/AppDbContext.cs
@@ -1,9 +1,5 @@
 ﻿using B1_task2.Server.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Security.Claims;
-using System.Security.Principal;
-using System.Transactions;
 using File = B1_task2.Server.Models.File;
 namespace B1_task2.Server.Data;
 public class AppDbContext : DbContext
@@ -14,9 +10,4 @@ public class AppDbContext : DbContext
     public DbSet<Balance> Balances { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-    /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=b1_task1;Username=postgres;Password=postgres;");
-    }*/
 }
